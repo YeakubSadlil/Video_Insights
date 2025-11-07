561633a258b859fd28bbb5c63777fbf04a12b0db
Yeakub_Sadlil <yakubsadlil@gmail.com>
2025-11-07 19:57:35 +0600

    test pytesseract and easyocr performance

--- feat_extractor.py
+++ feat_extractor.py
@@ -1,5 +1,7 @@
 import cv2
 import sys
+import easyocr
+import pytesseract
 
 def detect_shot_cuts(video_path):
     cap = cv2.VideoCapture(video_path)
@@ -31,8 +33,35 @@ def detect_shot_cuts(video_path):
     cap.release()
     return shot_counter
 
+def ocr_text_ratio(video_path):
+
+    """ test pytesseract and easyocr performance """
+
+    cap = cv2.VideoCapture(video_path)
+    reader = easyocr.Reader(['en'])
+    for i in range(50):
+        ret, frame = cap.read()
+        if ret:
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # x, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+            text = pytesseract.image_to_string(gray)
+            text1 = reader.readtext(gray)
+            print(f"text for {i} :", text)
+            for (bbox, text2, confidence) in text1:
+                print(f"Text eocr {i}: {text2}, Confidence: {confidence:.2f}")
+
+            cv2.imshow(f"gray {i}", gray)
+            cv2.waitKey(0)
+            cv2.destroyWindow(f"gray {i}")
+            # plt.plot(thresh)
+            # cv2.imshow(f"thresh {i}",thresh)
+            # cv2.waitKey(0)
+            # cv2.destroyWindow(f"thresh {i}")
+    cap.release()
+
 def video_extractor(video_path):
-    print("Extracting shot cuts")
+    print("Extracting shot cuts .....")
     shot_cuts = detect_shot_cuts(video_path)
     return shot_cuts
 
