22ecad8199102a5857e3d8946b3ebc5b2448a866
Yeakub_Sadlil <yakubsadlil@gmail.com>
2025-11-07 10:55:41 +0600

    initialization

---
+++ feat_extractor.py
@@ -0,0 +1,23 @@
+import cv2
+import sys
+
+def detect_shot_cuts(video_path):
+    pass
+
+def video_extractor(video_path):
+    print("Extracting shot cuts")
+    shot_cuts = detect_shot_cuts(video_path)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        print("Usage: python shot_cuts.py <path to video>")
+        sys.exit(1)
+
+    video_path = sys.argv[1]
+
+    try:
+        feature = video_extractor(video_path)
+    except Exception as e:
+         print(f"error : {e}")
+         sys.exit(1)
