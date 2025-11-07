07681cc567d1f2ab389d140d976dd20bd1bffd75
Yeakub_Sadlil <yakubsadlil@gmail.com>
2025-11-07 11:32:27 +0600

    add shot_cut func

--- feat_extractor.py
+++ feat_extractor.py
@@ -2,11 +2,39 @@ import cv2
 import sys
 
 def detect_shot_cuts(video_path):
-    pass
+    cap = cv2.VideoCapture(video_path)
+    if not cap.isOpened():
+        raise FileNotFoundError(f"The video cannot be opened from : {video_path}")
+
+    ret, frame = cap.read()
+    if not ret:
+        cap.release()
+        return 0
+
+    previous_gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    previous_hist = cv2.calcHist([previous_gray],[0],None,[256],[0,256])
+    shot_counter = 0
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        hist = cv2.calcHist([gray], [0], None, [256], [0, 256])
+        diff = cv2.compareHist(hist,previous_hist,cv2.HISTCMP_CORREL)
+        threshold = 0.7
+        if diff < threshold:
+            shot_counter += 1
+        previous_hist = hist
+
+    cap.release()
+    return shot_counter
 
 def video_extractor(video_path):
     print("Extracting shot cuts")
     shot_cuts = detect_shot_cuts(video_path)
+    return shot_cuts
 
 if __name__ == "__main__":
 
@@ -18,6 +46,7 @@ if __name__ == "__main__":
 
     try:
         features = video_extractor(video_path)
+        print(features)
     except Exception as e:
          print(f"error : {e}")
          sys.exit(1)
